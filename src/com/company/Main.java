df5d52b5fc7e48956f8e599628526a8e12199ae1
Antonio Forrest <antonio.forrest.2023@student.lhschools.org>
2020-02-25 13:12:21 -0500

    Intellij Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("+----------------------------+");
+        System.out.println("|Pro Fortnite Gamers Pump    |");
+        System.out.println("|92012 your mom st           |");
+        System.out.println("|Columbus, Ohio              |");
+        System.out.println("|                            |");
+        System.out.println("|DATE       TIME             |");
+        System.out.println("|1/15/2020  3:01am           |");
+        System.out.println("|                            |");
+        System.out.println("|Total Owed: $58.38          |");
+        System.out.println("|                            |");
+        System.out.println("|Pump: 07                    |");
+        System.out.println("|Fuel: premium               |");
+        System.out.println("|                            |");
+        System.out.println("|Gallons: 41.7               |");
+        System.out.println("|Gallons/dollar: 1.4 gallons |");
+        System.out.println("|                            |");
+        System.out.println("|Thank you for your purchase!|");
+        System.out.println("|IMPORTANT retain for rewards|");
+        System.out.println("+----------------------------+");
+
+    }
+
+}
